a79308f24fca0f3dd2cda6b4820f251412795ca5
James <james.ide775@gmail.com>
2022-08-27 18:31:58 +0930

    Fix href

--- pages/index.js
+++ pages/index.js
@@ -132,7 +132,7 @@ export default function Home() {
                 <p className="p-2 hover:cursor-pointer hover:text-[#BED600] duration-500">
                   <a
                     href="https://user-images.githubusercontent.com/50192239/138374096-c028b072-ca7d-40af-86f5-4b9804134cd6.gif"
-                    target="_blank"
+                    target="#"
                   >
                     Demo
                   </a>
