e5d11c5c00b2e00b9bb20a0947fa11b8e8e75127
James <james.ide775@gmail.com>
2022-12-03 16:52:23 +1030

    Update conduit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import MainImage from "../assets/main.png"
-import Image from "next/dist/client/image"
-import Layout from "../components/Layout"
+import MainImage from "../assets/main.png";
+import Image from "next/dist/client/image";
+import Layout from "../components/Layout";
 export default function Home() {
   return (
     <div>
@@ -34,22 +34,12 @@ export default function Home() {
                   Contact
                 </a>
               </div>
-              <p className="text-gray-700 dark:text-gray-200 mt-3 mb-4">
-                Consultant at{" "}
-                <a
-                  className="font-bold hover:cursor-pointer hover:text-[#BED600] duration-500"
-                  href="https://chamonix.com.au/"
-                  target="#"
-                >
-                  Chamonix IT Solutions
-                </a>
-              </p>
-              <p className="max-w-md text-gray-700">
+              <p className="max-w-md text-gray-700 mt-3">
                 Developer from South Australia. I aim to deepen my understanding
                 in various areas of software and web development.
               </p>
             </div>
-            <div className="relative mb-2  xl:ml-auto md:ml-auto mr-auto mt-3">
+            <div className="relative mb-2 xl:ml-auto md:ml-auto mr-auto">
               <Image
                 src={MainImage}
                 alt="James Ide"
@@ -61,6 +51,24 @@ export default function Home() {
             </div>
           </div>
           <div className=" xl:p-1 md:p-1 p-4">
+            <h2 className="font-bold text-3xl md:text-4xl tracking-tight mb-1 text-black dark:text-white">
+              Employment
+            </h2>
+            <div className="flex flex-row justify-between max-w-xl xl:p-1 md:p-1 p-4">
+              <p className="text-gray-700 dark:text-gray-200 mt-3 mb-4">
+                Consultant at{" "}
+                <a
+                  className="font-bold hover:cursor-pointer hover:text-[#BED600] duration-500"
+                  href="https://chamonix.com.au/"
+                  target="#"
+                >
+                  Chamonix IT Solutions
+                </a>
+              </p>
+              <p className="text-gray-700 dark:text-gray-200 mt-3 mb-4">
+                July 2022 -
+              </p>
+            </div>
             <h2 className="font-bold text-3xl md:text-4xl tracking-tight mb-1 text-black dark:text-white">
               Education
             </h2>
@@ -91,29 +99,33 @@ export default function Home() {
               <div>
                 <h2 className="font-bold mt-4">Conduit</h2>
                 <p className="text-gray-700 max-w-md mt-2 mb-1">
-                  An article sharing platform, similar to Medium.
-                  </p>
-                  <p className="text-gray-700 max-w-md mt-2 mb-1">
-                    JWT authentication with access tokens, refresh tokens, and token revoking capability.
-                  </p>
-                  <p className="text-gray-700 max-w-md mt-2 mb-1">
-                  Relational features such as following users, liking articles, and commenting on articles. 
-                  </p>
-                  <p className="text-gray-700 max-w-md mt-2 mb-1">
-                  Server-side pagination and caching to reduce latency and improve performance.
-                  </p>
-                  <p className="text-gray-700 max-w-md mt-2 mb-1">
-                  Continuous Deployment (CD) using Google Cloud Build to build and deploy Docker containers to Google Cloud Run.
-                  </p>
-
+                  An scaleable article sharing platform, similar to Medium.
+                </p>
+                <p className="text-gray-700 max-w-md mt-2 mb-1">
+                  JWT authentication with access tokens, refresh tokens, and
+                  token revoking capability.
+                </p>
+                <p className="text-gray-700 max-w-md mt-2 mb-1">
+                  Relational features such as following users, liking articles,
+                  and commenting on articles.
+                </p>
+                <p className="text-gray-700 max-w-md mt-2 mb-1">
+                  Server-side pagination and caching to reduce latency and
+                  improve performance.
+                </p>
+                <p className="text-gray-700 max-w-md mt-2 mb-1">
+                  Deployed as a Kubernetes Cluster to Google Kubernetes Engine
+                  with an Nginx Ingress Controller for load balancing, routing
+                  and serving static files.
+                </p>
                 <p className="font-bold text-md">Technology</p>
                 <p className="text-md italic">
-                  React, NestJS, PostgreSQL, TypeORM, Docker, TailwindCSS 
+                  React, NestJS, PostgreSQL, Docker, Kubernetes, Nginx
                 </p>
               </div>
               <div className="inline-flex">
                 <span className="p-2 hover:cursor-pointer hover:text-[#BED600] duration-500">
-                  <a href="https://conduit-nu.vercel.app/" target="#">
+                  <a href="https://conduits.ml" target="#">
                     Live{" "}
                   </a>
                 </span>
@@ -133,13 +145,12 @@ export default function Home() {
                   Express.
                 </p>
                 <p className="text-gray-700 max-w-md mt-2 mb-1">
-                A TypeScript application where
-                  users can view and book hotel rooms and make payments with
-                  Stripe Payment API.
+                  A TypeScript application where users can view and book hotel
+                  rooms and make payments with Stripe Payment API.
                 </p>
                 <p className="text-gray-700 max-w-md mt-2 mb-1">
-                Implemented user authentication and
-                  authorization with access tokens, refresh tokens, and cookies.
+                  Implemented user authentication and authorization with access
+                  tokens, refresh tokens, and cookies.
                 </p>
                 <p className="font-bold text-md">Technology</p>
                 <p className="text-md italic">
@@ -165,16 +176,15 @@ export default function Home() {
                 <h2 className="font-bold mt-4">YouTube Trender</h2>
                 <p className="text-gray-700 max-w-md mt-2 mb-1">
                   An interactive application that parses JSON metadata about
-                  Youtube videos. 
+                  Youtube videos.
                 </p>
                 <p className="text-gray-700 max-w-md mt-2 mb-1">
-                It uses maps, lists and comparators to sort,
-                  search and index the data. 
+                  It uses maps, lists and comparators to sort, search and index
+                  the data.
                 </p>
                 <p className="text-gray-700 max-w-md mt-2 mb-1">
-                A GUI using the Java Swing
-                  framework allows the user to interact with the underlying data
-                  structures.
+                  A GUI using the Java Swing framework allows the user to
+                  interact with the underlying data structures.
                 </p>
                 <p className="font-bold text-md">Technology</p>
                 <p className="text-md italic">Java</p>
@@ -203,5 +213,5 @@ export default function Home() {
         </div>
       </Layout>
     </div>
-  )
+  );
 }
